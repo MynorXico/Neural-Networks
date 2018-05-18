7c179f4e3aa63ea103adcc0313bc9d57b00e5753
Mynor Xico <mynorottoniel.xico2@gmail.com>
2018-05-17 21:58:15 -0600

    Simple captcha defeater

--- Perceptrons.py
+++ Perceptrons.py
@@ -1,7 +1,9 @@
 """ Perceptrons"""
-from numpy import dot
+def dot(v, w):
+    """v_1 * w_1 + ... + v_n * w_n"""
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
 import math
-
+import random
 def step_function(x):
     return 1 if x >= 0 else 0
 
@@ -54,18 +56,113 @@ def backpropagate(network, input_vector, targets):
                       for output, target in zip(outputs, targets)]
 
     #adjust weights for output layer, one neuron at a time
-    for i, output_neuron in enumerate(network[1]):
+    for i, output_neuron in enumerate(network[-1]):
         # focus on the ith output layer neuron
         for j, hidden_output in enumerate(hidden_outputs + [1]):
             #adjust the jth weight based on both
             # this neuron's delta and its jth input
             output_neuron[j] -= output_deltas[i]*hidden_output
 
-        # back-propagate errors to hidden layer
-        hidden_deltas = [hidden_output*(1-hidden_output)*
+    # back-propagate errors to hidden layer
+
+    hidden_deltas = [hidden_output*(1-hidden_output)*
                          dot(output_deltas, [n[i] for n in output_layer])
                          for i, hidden_output in enumerate(hidden_outputs)]
         # adjust weights for hidden layer, one nueron at a time
-        for i, hidden_neuron in enumerate(network[0]):
-            for j, input in enumerate(input_vector + [1]):
-                hidden_neuron[j]-=hidden_deltas[i] * input
+    for i, hidden_neuron in enumerate(network[0]):
+        for j, input in enumerate(input_vector + [1]):
+            hidden_neuron[j]-=hidden_deltas[i] * input
+
+""" building our neural network for captcha recognizer """
+targets = [[1 if i == j else 0 for i in range(10)]
+           for j in range(10)]
+
+random.seed(0)  # to get repeatable results
+input_size = 25 # each input is a vector of length 25
+num_hidden = 5  # we'll have 5 neurons in the hidden layer
+output_size = 10# we need 10 outputs for each input
+
+# each hidden neuron has one wieght per input, plus a bias weight
+hidden_layer = [[random.random() for _ in range(input_size +1 )
+                 for _ in range(num_hidden)]]
+
+# each output neuron has one weight per hidden neuron, plus a bias weight
+output_layer = [[random.random() for _ in range(num_hidden +1)]
+                for _ in range(output_size)]
+
+# the network starts out with random weights
+network = [hidden_layer, output_layer]
+
+
+inputs = [[ 1,1,1,1,1,
+            1,0,0,0,1,
+            1,0,0,0,1,
+            1,0,0,0,1,
+            1,1,1,1,1],
+          [ 0,0,1,0,0,
+            0,0,1,0,0,
+            0,0,1,0,0,
+            0,0,1,0,0,
+            0,0,1,0,0],
+          [ 1,1,1,1,1,
+            0,0,0,0,1,
+            1,1,1,1,1,
+            1,0,0,0,0,
+            1,1,1,1,1],
+          [ 1,1,1,1,1,
+            0,0,0,0,1,
+            1,1,1,1,1,
+            0,0,0,0,1,
+            1,1,1,1,1],
+          [ 1,0,0,0,1,
+            1,0,0,0,1,
+            1,1,1,1,1,
+            0,0,0,0,1,
+            0,0,0,0,1,],
+          [ 1,1,1,1,1,
+            1,0,0,0,0,
+            1,1,1,1,1,
+            0,0,0,0,1,
+            1,1,1,1,1],
+          [ 1,1,1,1,1,
+            1,0,0,0,0,
+            1,1,1,1,1,
+            1,0,0,0,1,
+            1,1,1,1,1],
+          [ 1,1,1,1,1,
+            0,0,0,0,1,
+            0,0,0,0,1,
+            0,0,0,0,1,
+            0,0,0,0,1],
+          [ 1,1,1,1,1,
+            1,0,0,0,1,
+            1,1,1,1,1,
+            1,0,0,0,1,
+            1,1,1,1,1],
+          [ 1,1,1,1,1,
+            1,0,0,0,1,
+            1,1,1,1,1,
+            0,0,0,0,1,
+            1,1,1,1,1]]
+
+
+""" Lets train our neural network"""
+for _ in range(10000):
+    for input_vector, target_vector in zip(inputs, targets):
+        backpropagate(network, input_vector, target_vector)
+
+print("Success!")
+
+def predict(input):
+    return feed_forward(network, input)[-1]
+
+answer = predict([0,1,1,1,0, # .@@@.
+                1,0,0,1,1, # @..@@
+                0,1,1,1,0, # .@@@.
+                1,0,0,1,1, # @..@@
+                0,1,1,1,0]) # .@@@.
+
+for x in range(len(answer)):
+    answer[x] = round(answer[x]/max(answer),2)
+
+print(answer)
